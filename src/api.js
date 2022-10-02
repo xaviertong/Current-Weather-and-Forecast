ffe4dcddb2108d7cffa01d2a6805e9a686c66386
Xiaoxiang Tong <110347779+xaviertong@users.noreply.github.com>
2022-10-01 20:12:02 -0500

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "e0472d0f2dmshe506a06878f0bcfp192807jsncc31a64ca4a6",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "134deb1946185dbcc9fde6d628d6734f";
+
